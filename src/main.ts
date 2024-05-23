eed1a48c80734e89d737e4527a2b19c08e5ae3fd
Setas8 <dcsetas@gmail.com>
2024-05-23 15:02:25 +0200

    api connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  if (process.env.NODE_ENV === 'development') {
+    const config = new DocumentBuilder()
+      .setTitle('Clinic API')
+      .setDescription('Clinic REST API')
+      .setVersion('1.0')
+      .addTag('HealthCheck', 'endpoint to verify API status')
+      .addTag('Patients', 'Endpoints for patients resource')
+      .addTag('Insurances', 'Endpoints insurance resources')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+  app.setGlobalPrefix('v1');
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.API_PORT);
 }
 bootstrap();
